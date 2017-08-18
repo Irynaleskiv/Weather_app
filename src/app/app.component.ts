d5f908f89e144b634680d7b26cc420deb2c380cf
Iryna Leskiv <irynaleskiv@online.ua>
2017-08-19 02:17:04 +0300

    defined the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private http: Http) { }
+
+  newDate;
+  cityName;
+  humidity;
+  location;
+  summary;
+  pressure;
+  tempK;
+  tempC;
+  rain;
+  fullImagePath;
+
+    ngOnInit() {
+      this.geoFind();
+      setTimeout(() => {
+        this.searchCity();
+     }, 500);   
+    }
+
+    geoFind() {
+      this.http.get('https://ipinfo.io')
+      .subscribe (
+      (res: Response) => {
+      const currCity = res.json();
+      this.cityName = currCity.city;
+    });
+  }
+
+    searchCity() {
+      this.newDate = new Date().toDateString();
+      this.fullImagePath = './assets/11.png';
+      this.https.get('http://api.openweathermap.org/data/2.5/weather?APPID=0ba5e82df49a15a79cec569618c56215&q=' + this.cityName)
+      .subscribe (
+      (res: Response) => {
+      const weatherCity = res.json();
+      this.humidity = `Humidity: ${weatherCity.main.humidity}%`;
+      this.pressure = `Pressure: ${weatherCity.main.pressure} hPa`;
+      this.location = weatherCity.name + ', ' + weatherCity.sys.country;
+      this.summary = weatherCity.weather[0].main;
+      this.tempK = weatherCity.main.temp;
+      this.tempC = `${Math.round(this.tempK - 273.15)}°C`;
+
+      if(this.summary === 'Rain') {
+        this.fullImagePath = './assets/14.png';
+      }
+    });
+  }
 }
+
+
+
